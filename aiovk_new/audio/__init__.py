d6dbf2d1bccfa9c88fe0c7f24cf056509b5aaa73
Daniel Zakharov <daniel734@bk.ru>
2022-01-21 13:46:08 +0300

    Use env instead of hardcoded tokens (#1)
    * Use env instead of hardcoded tokens
    * Hide token in repr
    * Remove bandit from dev-dependencies
    * anyurl => str

--- aiovk_new/audio/__init__.py
+++ aiovk_new/audio/__init__.py
@@ -1,10 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from ..base import BaseMethod
 
 
 @dataclass
 class Audio(BaseMethod):
-    access_token: str
+    access_token: str = field(repr=None)
     api_version: str = "5.131"
 
     from .search import search
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -13,7 +13,7 @@ async def test_audio_get(aiovk: AioVK):
 
 @pytest.mark.asyncio
 async def test_audio_search(aiovk: AioVK):
-    song, *_ = await aiovk.audio.search("Rick Astley - Never Gonna Give You Up")
+    songs = await aiovk.audio.search("Rick Astley - Never Gonna Give You Up")
+    common_info = [(song.artist, song.title) for song in songs]
 
-    assert song.artist == "Rick Astley"
-    assert song.title == "Never Gonna Give You Up"
+    assert any([song == ("Rick Astley", "Never Gonna Give You Up") for song in common_info])

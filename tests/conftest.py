873fd145c44f2a639a019680f8dd61076816e41e
Daniel Zakharov <daniel734@bk.ru>
2022-01-21 12:08:03 +0300

    Use env instead of hardcoded tokens

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from aiovk_new import AioVK
 
+import os
+
 
 @pytest.fixture(scope="function")
 def aiovk():
-    return AioVK(access_token="80f76354d97fdb2efc9811073065e12279266428374048b03db27299b47c45b93dce1cda085c2afaa85c4")
+    return AioVK(access_token=os.environ["ACCESS_TOKEN"])
